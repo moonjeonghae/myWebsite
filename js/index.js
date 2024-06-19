8067ddf72f09a625a057a2e4c37ca9485f8e7a6f
moonjeonghae <wjdgo8@naver.com>
2024-06-19 19:26:12 +0900

    [06/19] 1925L

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ window.onload = function() {
     const htmlClick = document.querySelector('.icon-box > .html');
     const htmlModal = document.querySelector('.skill-desc .html')
     const skillDesc = document.querySelector('.skill-desc');
-    
+    const xBtn = document.querySelector('.x-btn');
     // # icon click 시
     htmlClick.addEventListener('click', () => {
         // # click한 icon과 관련 있는 skill-desc-txt가 뜸
@@ -35,7 +35,10 @@ window.onload = function() {
         // # scroll 없앨 것이기 때문에 body에 있는 .scroll-stop 추가
         $body.classList.add('scroll-stop');
     });
-
-
     // X 버튼 누르면 모달이 사라짐
+    xBtn.addEventListener('click', () => {
+        skillDesc.style.display = 'none';
+        htmlModal.style.display = 'none';
+        $body.classList.remove('scroll-stop');
+    });
 };
