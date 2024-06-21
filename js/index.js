54109b5ffe322ef7c76176a6b09d1d67d8ca7124
moonjeonghae <wjdgo8@naver.com>
2024-06-21 17:44:52 +0900

    [06/21] 1744L

--- js/index.js
+++ js/index.js
@@ -22,23 +22,38 @@ window.onload = function() {
 
 
     // ----- 모바일 modal -----
-    const $body = document.querySelector('body');
-    const htmlClick = document.querySelector('.icon-box > .html');
-    const htmlModal = document.querySelector('.skill-desc .html')
-    const skillDesc = document.querySelector('.skill-desc');
-    const xBtn = document.querySelector('.x-btn');
-    // # icon click 시
-    htmlClick.addEventListener('click', () => {
-        // # click한 icon과 관련 있는 skill-desc-txt가 뜸
-        skillDesc.style.display = 'block';
-        htmlModal.style.display = 'block';
-        // # scroll 없앨 것이기 때문에 body에 있는 .scroll-stop 추가
-        $body.classList.add('scroll-stop');
-    });
-    // X 버튼 누르면 모달이 사라짐
-    xBtn.addEventListener('click', () => {
-        skillDesc.style.display = 'none';
-        htmlModal.style.display = 'none';
-        $body.classList.remove('scroll-stop');
-    });
+
+    // innerWidth 이용해서 모바일만 적용되게 하기 !!!!!!!!!!!!
+    var windowWidth = window.innerWidth;
+
+    if(windowWidth < 768 ) {
+        const $body = document.querySelector('body');
+        const iconClick = document.querySelectorAll('.icon-box > .icon');
+        const iconModal = document.querySelectorAll('.skill-desc .skill-desc-txt')
+        const skillDesc = document.querySelector('.skill-desc');
+        const xBtn = document.querySelector('.x-btn');
+    
+        // #icon click 시 발생할 함수 만들기
+        const showModal = (idx) => {
+            skillDesc.style.display = 'block';
+            iconModal[idx].style.display = 'block';
+            // # scroll 없앨 것이기 때문에 body에 있는 .scroll-stop 추가
+            $body.classList.add('scroll-stop');
+        }
+    
+        // # 각 icon에 함수 적용
+        iconClick.forEach((icon, idx) => {
+            icon.addEventListener('click', () => showModal(idx))
+        })
+    
+        // X 버튼 누르면 모달이 사라지는 함수 만들기
+        const removeModal = () => {
+            skillDesc.style.display = 'none';
+            iconModal.forEach(modal => modal.style.display = 'none');
+            $body.classList.remove('scroll-stop');
+        }
+    
+        // # button에 함수 적용하기
+        xBtn.addEventListener('click', removeModal);
+    }
 };
