1389f474c914efd60bb214de43a57bdb220accdf
moonjeonghae <wjdgo8@naver.com>
2024-06-24 08:10:18 +0900

    주말 수정

--- js/index.js
+++ js/index.js
@@ -1,34 +1,123 @@
 window.onload = function() {
-
-
-
-    //====== skill ======
-    // ----- 모바일 skill btn -----
-    const donuts = document.querySelectorAll('.icon');
-
-    donuts.forEach(donut => {
-        const ttlPercent = parseFloat(donut.dataset.deg);
-        let final = 0;
-        const donutAnimation = setInterval(() => {
-            donut.dataset.deg = final;
-            donut.style.background = `conic-gradient(#efb1d5 0% ${final}%, #fff ${final}% 100%)`;
-
-            final++;
-            if (final >= ttlPercent) {
-                clearInterval(donutAnimation);
-            }
-        }, 10);
-    });
-
-
-    // ----- 모바일 modal -----
-
     // innerWidth 이용해서 모바일만 적용되게 하기 !!!!!!!!!!!!
     var windowWidth = window.innerWidth;
 
+    //====== about me ======
+    // ----- aboutme-tab-btn click 시 -----
+    // ***** highlight css 적용 *****
+    // ***** 해당 내용 나오게 하기 *****
+
+
+    //====== skill ======
+    // ----- [모바일] tabBtn click 시 -----
+    if(windowWidth < 768) {
+        const skillTabBtn = document.querySelectorAll('.skill-tab-btn button');
+        const allBtn = document.querySelector('.skill-tab-btn .all');
+        const codingBtn = document.querySelector('.skill-tab-btn .coding');
+        const designBtn = document.querySelector('.skill-tab-btn .design');
+        const videoBtn = document.querySelector('.skill-tab-btn .video');
+        const etcBtn = document.querySelector('.skill-tab-btn .etc');
+        const codingIcon = document.querySelectorAll('.icon-box .coding');
+        const designIcon = document.querySelectorAll('.icon-box .design');
+        const videoIcon = document.querySelector('.icon-box .video');
+        const etcIcon = document.querySelector('.icon-box .etc');
+
+        // ***** selected css 적용 *****
+
+        const selectedBtn = (e) => {
+            allBtn.classList.remove('selected');
+            skillTabBtn.forEach(btn => btn.classList.remove('selected'));
+            e.target.classList.add('selected');
+        };
+
+        skillTabBtn.forEach(btn => {
+            btn.addEventListener('click', selectedBtn);
+        });
+
+
+        // ***** 해당 icon만 보이게 하기 *****
+        allBtn.addEventListener('click', function() {
+            codingIcon.forEach(icon => {
+                icon.style.display = 'block';
+            });
+            designIcon.forEach(icon => {
+                icon.style.display = 'block';
+            });
+            videoIcon.style.display = 'block';
+            etcIcon.style.display = 'block';
+        });
+
+        codingBtn.addEventListener('click', function() {
+            codingIcon.forEach(icon => {
+                icon.style.display = 'block';
+            });
+            designIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            videoIcon.style.display = 'none';
+            etcIcon.style.display = 'none';
+        });
+
+        designBtn.addEventListener('click', function() {
+            codingIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            designIcon.forEach(icon => {
+                icon.style.display = 'block';
+            });
+            videoIcon.style.display = 'none';
+            etcIcon.style.display = 'none';
+        });
+
+        videoBtn.addEventListener('click', function() {
+            codingIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            designIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            videoIcon.style.display = 'block';
+            etcIcon.style.display = 'none';
+        });
+
+        etcBtn.addEventListener('click', function() {
+            codingIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            designIcon.forEach(icon => {
+                icon.style.display = 'none';
+            });
+            videoIcon.style.display = 'none';
+            etcIcon.style.display = 'block';
+        });
+    }
+
+
+    // ----- [모바일] icon graph -----
+    if(windowWidth < 768) {
+        const donuts = document.querySelectorAll('.icon');
+    
+        donuts.forEach(donut => {
+            const ttlPercent = parseFloat(donut.dataset.deg);
+            let final = 0;
+            const donutAnimation = setInterval(() => {
+                donut.dataset.deg = final;
+                donut.style.background = `conic-gradient(#efb1d5 0% ${final}%, #fff ${final}% 100%)`;
+    
+                final++;
+                if (final >= ttlPercent) {
+                    clearInterval(donutAnimation);
+                }
+            }, 10);
+        });
+    }
+
+
+    // ----- [모바일] modal -----
+    const $body = document.querySelector('body');
+    const icons = document.querySelectorAll('.icon-box > .icon');
+
     if(windowWidth < 768 ) {
-        const $body = document.querySelector('body');
-        const iconClick = document.querySelectorAll('.icon-box > .icon');
         const iconModal = document.querySelectorAll('.skill-desc .skill-desc-txt')
         const skillDesc = document.querySelector('.skill-desc');
         const xBtn = document.querySelector('.x-btn');
@@ -42,7 +131,7 @@ window.onload = function() {
         }
     
         // # 각 icon에 함수 적용
-        iconClick.forEach((icon, idx) => {
+        icons.forEach((icon, idx) => {
             icon.addEventListener('click', () => showModal(idx))
         })
     
@@ -56,4 +145,39 @@ window.onload = function() {
         // # button에 함수 적용하기
         xBtn.addEventListener('click', removeModal);
     }
+
+
+    // ----- [PC] icon 배치 -----
+    if(windowWidth > 768) {
+        const radius = 300;
+
+        const iconArrangement = (idx) => {
+            // #각도를 라디안으로 변환
+            const angle = (idx / icons.length) * 2 * Math.PI;
+            // #(x,y) 좌표 구하기
+            const x = radius * Math.cos(angle);
+            const y = radius * Math.sin(angle);
+            // #계산된 좌표로 각 div 이동하기
+            const $icon = icons[idx];
+            $icon.style.left = `calc(50% + ${x}px)`;
+            $icon.style.top = `calc(50% + ${y}px)`;
+        };
+
+        // # 각 icon 박스에 적용시키기
+        icons.forEach((icon, idx) => {
+            iconArrangement(idx);
+        });
+    }
+
+
+    //====== contact ======
+    // ----- focus 시 placeholder 없애기 -----
+    // const inputs = document.querySelectorAll('input');
+    // const textarea = document.querySelector('textarea');
+
+    // inputs.forEach(input => {
+    //     input.addEventListener('focus', function() {
+    //         this.setAttribute('unvisible', this.setAttribute('visible'));
+    //     })
+    // })
 };
