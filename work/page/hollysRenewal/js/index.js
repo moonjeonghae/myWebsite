4f4ba5faff17ce5cc7ec1ee5e4feee31d752660b
moonjeonghae <wjdgo8@naver.com>
2024-09-18 17:37:59 +0900

    점검 후 수정

--- work/page/hollysRenewal/js/index.js
+++ work/page/hollysRenewal/js/index.js
@@ -1,25 +1,118 @@
 window.onload = function() {
     const slideImg = document.querySelector('.slide-image');
-    const banner = document.querySelectorAll('.banner');
-    const slideWidth = banner[0].clientWidth;
-    let slideClone = banner[0].cloneNode(true);
-    let slideCount = banner.length;
+    const banners = document.querySelectorAll('.banner');
+    const bannerBtns = document.querySelectorAll('.banner-btn');
+    const slideWidth = banners[0].clientWidth;
+    let slideClone = banners[0].cloneNode(true);
+    let slideCount = banners.length;
     let currentIdx = 0;
 
     slideImg.appendChild(slideClone);
 
-    setInterval(() => {
-        currentIdx++;
-
+    function updateBanner() {
         slideImg.style.transition = 'transform 0.5s';
         slideImg.style.transform = `translateX(-${slideWidth * currentIdx}px)`;
 
-        if (currentIdx === slideCount) {
-            setTimeout(() => {
-                slideImg.style.transition = 'none';
-                slideImg.style.transform = 'translateX(0)';
-                currentIdx = 0;
-            }, 500);
+        bannerBtns.forEach((btn, idx) => {
+            btn.classList.toggle('active', idx === currentIdx);
+        });
+    }
+
+    let slideSetInterval;
+
+    function startSlideShow() {
+        if(slideSetInterval) {
+            clearInterval(slideSetInterval)
         }
-    }, 3000)
+
+        slideSetInterval = setInterval(() => {
+
+            currentIdx++;
+    
+            updateBanner();
+    
+            if (currentIdx === slideCount) {
+                bannerBtns[0].classList.add('active');
+                setTimeout(() => {
+                    slideImg.style.transition = 'none';
+                    slideImg.style.transform = 'translateX(0)';
+                    currentIdx = 0;
+                }, 500);
+            }
+        }, 3000)
+    }
+
+    startSlideShow();
+
+    bannerBtns.forEach((btn, idx) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            currentIdx = idx;
+            updateBanner();
+            startSlideShow();
+        });
+    });
+
+
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
+    const menuList = document.querySelector('.menu-list ul');
+    const moveWidth = 281;
+    const maxStep = 2;
+    let step = 0;
+
+    rightArrow.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(maxStep > step) {
+            step++;
+            rightMove();
+        }
+    });
+
+    leftArrow.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(step > 0) {
+            step--;
+            rightMove();
+        }
+    });
+
+    function rightMove() {
+        const position = moveWidth * step;
+        menuList.style.transition = 'transform 0.5s';
+        menuList.style.transform = `translateX(-${position}px)`;
+    }
+
+
+    const menuItems = document.querySelectorAll('.menu-list li > a');
+    menuItems.forEach(item => {
+        item.addEventListener('click', (e) => e.preventDefault());
+    });
+
+    const newsTitles = document.querySelectorAll('.news h3 > a');
+    const event = document.querySelector('.event > h3 > a');
+    const sns = document.querySelector('.sns > h3 > a');
+    const eventContent = document.querySelector('.event > ul');
+    const snsContent = document.querySelector('.sns > ul');
+
+    newsTitles.forEach(title => {
+        title.addEventListener('click', () => {
+            newsTitles.forEach(title => {
+                title.classList.remove('selected');
+            });
+            title.classList.add('selected');
+        });
+    });
+
+    event.addEventListener('click', (e) => {
+        e.preventDefault();
+        eventContent.style.display = 'flex';
+        snsContent.style.display = 'none';
+    });
+
+    sns.addEventListener('click', (e) => {
+        e.preventDefault();
+        snsContent.style.display = 'flex';
+        eventContent.style.display = 'none';
+    });
 }
